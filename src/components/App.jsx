e8c3779d03395391f46e03440ae876125ce18e9f
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2024-01-02 15:18:06 +0200

    converted into hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
@@ -12,90 +12,78 @@ const defaultContacts = [
   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
 ];
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
+export const App = () => {
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) ?? defaultContacts
+  );
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const stringifiedContacts = JSON.stringify(contacts);
+    localStorage.setItem('contacts', stringifiedContacts);
+  }, [contacts]);
+
+  const changeFilter = e => {
+    setFilter(e.target.value);
   };
 
-  changeFilter = e => {
-    this.setState({ filter: e.target.value });
-  };
-
-  addNewContact = (name, number) => {
-    const isInContacts = this.state.contacts.some(
+  const addNewContact = (name, number) => {
+    const isInContacts = contacts.some(
       contact => name.toLowerCase() === contact.name.toLowerCase()
     );
+    console.log('isInContacts: ', isInContacts);
 
     if (isInContacts) {
       alert(`${name} is already in contacts`);
       return;
     }
-    this.setState(prevState => ({
-      contacts: [
-        {
-          id: nanoid(),
-          name,
-          number,
-        },
-        ...prevState.contacts,
-      ],
-    }));
+
+    setContacts(prevState => [
+      {
+        id: nanoid(),
+        name,
+        number,
+      },
+      ...prevState,
+    ]);
   };
 
-  removeContact = contactId => {
-    this.setState(prevState => {
-      return {
-        contacts: prevState.contacts.filter(({ id }) => id !== contactId),
-      };
+  const removeContact = contactId => {
+    setContacts(prevState => {
+      console.log('prevState: ', prevState);
+      return prevState.filter(({ id }) => id !== contactId);
     });
   };
 
-  filterContacts = () => {
-    const { filter, contacts } = this.state;
+  const filterContacts = () => {
     const normalizeFilter = filter.trim().toLowerCase();
+    console.log('contacts: ', contacts);
 
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizeFilter)
     );
   };
 
-  componentDidMount() {
-    const stringifiedContacts = localStorage.getItem('contacts');
-    const contacts = JSON.parse(stringifiedContacts) ?? defaultContacts;
-    this.setState({ contacts });
-  }
+  const filterForContacts = filterContacts();
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      const stringifiedContacts = JSON.stringify(this.state.contacts);
-      localStorage.setItem('contacts', stringifiedContacts);
-    }
-  }
-
-  render() {
-    const { filter } = this.state;
-    const filterContacts = this.filterContacts();
-
-    return (
-      <div className={css.main}>
-        <h1>Phonebook</h1>
-        <ContactForm addNewContact={this.addNewContact} />
-        <h2>Contacts</h2>
-        {this.state.contacts.length > 0 ? (
-          <>
-            <Filter value={filter} onChangeFilter={this.changeFilter} />
-            <ContactList
-              contacts={filterContacts}
-              onRemoveContact={this.removeContact}
-            />
-          </>
-        ) : (
-          <p className={css.emptyInfo}>
-            Your phonebook is empty. Add first contact!
-          </p>
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className={css.main}>
+      <h1>Phonebook</h1>
+      <ContactForm addNewContact={addNewContact} />
+      <h2>Contacts</h2>
+      {contacts.length > 0 ? (
+        <>
+          <Filter value={filter} onChangeFilter={changeFilter} />
+          <ContactList
+            contacts={filterForContacts}
+            onRemoveContact={removeContact}
+          />
+        </>
+      ) : (
+        <p className={css.emptyInfo}>
+          Your phonebook is empty. Add first contact!
+        </p>
+      )}
+    </div>
+  );
+};
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,63 +1,69 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import css from './ContactForm.module.css';
 import { nanoid } from 'nanoid';
 
-export class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+export const ContactForm = ({ addNewContact }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  onSubmitForm = event => {
+  const onSubmitForm = event => {
     event.preventDefault();
 
     const form = event.currentTarget;
     const { name, number } = form.elements;
 
-    this.props.addNewContact(name.value, number.value);
-    this.resetForm();
+    addNewContact(name.value, number.value);
+    resetForm();
   };
 
-  handleChange = e => {
+  const handleChange = e => {
     const { name, value } = e.target;
-    this.setState({ [name]: value });
+
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        break;
+    }
   };
 
-  resetForm = () => {
-    this.setState({ name: '', number: '' });
+  const resetForm = () => {
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    const { name, number } = this.state;
-    return (
-      <form action="" onSubmit={this.onSubmitForm} className={css.formContact}>
-        <label className={css.formContactLable} htmlFor={nanoid()}>
-          Name
-          <input
-            type="text"
-            name="name"
-            value={name}
-            onChange={this.handleChange}
-            className={css.inputFormContact}
-            required
-          />
-        </label>
-        <label className={css.formContactLable} htmlFor={nanoid()}>
-          Number
-          <input
-            type="tel"
-            name="number"
-            value={number}
-            onChange={this.handleChange}
-            className={css.inputFormContact}
-            required
-          />
-        </label>
+  return (
+    <form action="" onSubmit={onSubmitForm} className={css.formContact}>
+      <label className={css.formContactLable} htmlFor={nanoid()}>
+        Name
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleChange}
+          className={css.inputFormContact}
+          required
+        />
+      </label>
+      <label className={css.formContactLable} htmlFor={nanoid()}>
+        Number
+        <input
+          type="tel"
+          name="number"
+          value={number}
+          onChange={handleChange}
+          className={css.inputFormContact}
+          required
+        />
+      </label>
 
-        <button className={css.addContactBtn} type="submit">
-          add contact
-        </button>
-      </form>
-    );
-  }
-}
+      <button className={css.addContactBtn} type="submit">
+        add contact
+      </button>
+    </form>
+  );
+};
